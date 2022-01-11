0a333892cb5509ab5894eae7c333e40e75bc253f
Kevin Wang <kevqwang@gmail.com>
2022-01-11 16:33:41 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(source, string) {
+    return source.filter(
+        (possible) => possible.toLowerCase() === string.toLowerCase ()
+    );
+}
+
+function fuzzyMatch(source, string) {
+    return source.filter(
+      (possible) =>
+        possible.toLowerCase().indexOf(string.toLowerCase()) === 0
+    );
+  }
+  
+  function matchName(source, stringName) {
+    return source.filter((record) => record.name === stringName);
+  }
